b3c588eb5a69d1b1d56bbd8b343a08698f228544
naghceuz <guanchenzhang@gmail.com>
2015-04-25 21:24:27 -0400

    fix the autocomplete bug

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/error', function(req, res, next) {
+  res.render('error');
+});
+
+
 module.exports = router;
